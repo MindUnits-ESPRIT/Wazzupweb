95088e67d3b17b8d8d65b088903c8d6c8e253268
FinalHexcor <58568739+FinalHexcor@users.noreply.github.com>
2022-04-10 09:32:52 +0100

    staching changes

--- src/Controller/ListCollabController.php
+++ src/Controller/ListCollabController.php
@@ -5,11 +5,14 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use App\Entity\SalleCollaboration;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Utilisateurs;
 class ListCollabController extends AbstractController
 {
     /**
-     * @Route("/list/collab", name="app_list_collab")
+     * @Route("/list/collab", name="app_list_collab"  , methods={"GET", "POST"})
      */
     public function index(): Response
     {
